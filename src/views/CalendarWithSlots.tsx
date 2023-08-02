3b5defda5c7ae8abfeb48f46f7616bc67730a610
jesusalc <jesusalc@gmail.com>
2023-08-02 02:05:21 +0200

    fixed showing proper time to all route

--- src/views/CalendarWithSlots.tsx
+++ src/views/CalendarWithSlots.tsx
@@ -32,7 +32,8 @@ const CalendarWithSlots: React.FC<CalendarWithSlotsProps> = ({
       .then((res) => res.json())
       .then((response: ApiResponse) => {
         if (isSuccessResponse(response)) {
-          const slotsData = response.data.slots as SlotsBooked; // Casting to the correct type
+          const slotsData = response.data.slots as SlotsBooked;
+          console.log('slotsData',slotsData)
           setSlots(slotsData);
           setDefaultSlots(slotsData);
         } else if (response.error) {
@@ -49,55 +50,18 @@ const CalendarWithSlots: React.FC<CalendarWithSlotsProps> = ({
       });
   }, []);
 
-
-
-
-  // Optionally, you can add the surrounding months based on your requirement
-  // For example, you can add one month before and after the earliest and latest months
-  // const surroundingMonths: Date[] = [];
-  // const prevMonth = new Date(earliestDate);
-  // const nextMonth = new Date(latestDate);
-  // prevMonth.setMonth(prevMonth.getMonth() - 1);
-  // nextMonth.setMonth(nextMonth.getMonth() + 1);
-  // surroundingMonths.push(prevMonth, ...months, nextMonth);
-
-  // Create the calendar layout using CSS Grid or Flexbox
-  // ... (continue with the calendar layout)
-
-  const generateWeeks = (month: Date): Date[][] => {
-    const weeks: Date[][] = [];
-    const startDate = startOfWeek(month);
-    const numWeeks = getWeeksInMonth(month);
-
-    for (let i = 0; i < numWeeks; i++) {
-      const week: Date[] = [];
-      for (let j = 0; j < 7; j++) {
-        const currentDate = addDays(startDate, i * 7 + j);
-        week.push(currentDate);
-      }
-      weeks.push(week);
-    }
-
-    return weeks;
-  };
-
-  // Function to filter slots for a specific date
-  const getSlotsForDate = (date: Date): SlotBooked[] => {
-    const dateString = format(date, 'yyyy-MM-dd HH:mm');
-    const slotEnd = date;
-    slotEnd.setMinutes(slotEnd.getMinutes() + 15);
-    const dateEndString = format(slotEnd, 'yyyy-MM-dd HH:mm');
-    return slots.filter((slot) => dateString >= slot.date && dateString <= dateEndString);
-  };
-
-  // Find the earliest and latest dates from the slots array
+  // Convert to Date object temporarily to find min/max dates
   const earliestDate = slots.reduce((minDate: Date | null, slot): Date | null => {
-    const slotDate = new Date(slot.date);
+    // const slotDate = new Date(slot.date);
+    const slotDate = parseISO(slot.date); // Replace with appropriate parsing if not ISO 8601 format
+
     return minDate ? (slotDate < minDate ? slotDate : minDate) : slotDate;
   }, null);
 
   const latestDate = slots.reduce((maxDate: Date | null, slot): Date | null => {
-    const slotDate = new Date(slot.date);
+    const slotDate = parseISO(slot.date); // Replace with appropriate parsing if not ISO 8601 format
+
+    // const slotDate = new Date(slot.date);
     const modifiedSlotDate = new Date(slotDate);
     modifiedSlotDate.setMinutes(modifiedSlotDate.getMinutes() + 15);
     return maxDate ? (modifiedSlotDate > maxDate ? modifiedSlotDate : maxDate) : modifiedSlotDate;
@@ -121,20 +85,6 @@ const CalendarWithSlots: React.FC<CalendarWithSlotsProps> = ({
   }, [earliestDate, latestDate]);
 
   console.log("months:", months);
-
-  const formatSlotCalendarTime = (dateStart: string, count: number) => {
-    const startDate = parseISO(dateStart);
-    const slotEnd = startDate;
-    slotEnd.setMinutes(slotEnd.getMinutes() + 15);
-    const dateEndString = format(slotEnd, 'yyyy-MM-dd HH:mm');
-    const endDate = parseISO(dateEndString);
-    const isSameDay = startDate.toDateString() === endDate.toDateString();
-    const formattedStartDate = format(startDate, 'MMM d, HH:mm');
-    const formattedEndDate = format(endDate, 'HH:mm');
-    return isSameDay
-      ? `${formattedStartDate} - ${formattedEndDate}`
-      : `${formattedStartDate} -- ${formattedEndDate}`;
-  };
 
   const currentDate = earliestDate ? new Date(earliestDate) : new Date();
   // Extract the date property from the slots array to use as bookedDates
